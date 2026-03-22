653427b30d73c5acbf43c324087f75d101080ca3
akishwe <avinashkishwe1@gmail.com>
2026-03-22 16:02:35 +0530

    Added more comments

--- python/reverse_string_1.py
+++ python/reverse_string_1.py
@@ -1,9 +1,14 @@
 def reverse_string(string):
+    # Create a variable to hold the left pointer
     left = 0
+    # Create a variable to hold the right pointer
     right = len(string) -1
 
+    # Loop until the left pointer is less than the right pointer
     while left < right:
+        # Swap the characters at the left and right pointers
         string[left], string[right] = string[right], string[left]
+        # Move the left pointer to the right and the right pointer to the left
         left += 1
         right -= 1
 
